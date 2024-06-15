ed6b199515edb636a63c8bb7db24e3f0d3b92d1f
cheng920611 <s1100124@gm.pu.edu.tw>
2024-06-15 08:04:14 +0800

    Add files via upload

--- 411001241.py
+++ 411001241.py
@@ -7,15 +7,15 @@ Created on Sat Jun 15 07:28:17 2024
 
 import os
 import haohaninfo
-from order_Lo8 import Record
-import numpy as np
+#from order_Lo8 import Record
+#import numpy as np
 from talib.abstract import SMA,EMA, WMA, RSI, BBANDS, MACD
 import sys
-import indicator_f_Lo2_short,datetime, indicator_forKBar_short
+#import indicator_f_Lo2_short,datetime, indicator_forKBar_short
 import pandas as pd
 import streamlit as st 
 import streamlit.components.v1 as stc 
-from order_streamlit import Record
+#from order_streamlit import Record
 import matplotlib.pyplot as plt
 import matplotlib
 
